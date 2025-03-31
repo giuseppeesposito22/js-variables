e5f1fa16780a5c1fd856f82f087d270b1c1e6ed1
giuseppeesposito22 <gius.espo22@gmail.com>
2025-03-31 16:03:35 +0200

    adjust method

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -6,17 +6,20 @@ const hello = "hello"; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = city.indexOf("Y");
+const cityIndex = city.indexOf("y".toLocaleUpperCase());
 console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = city.slice(city.indexOf("Y"));
+const citySubstring = city.slice(city.indexOf("y".toLocaleUpperCase()));
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = city.replace(city.slice(city.indexOf("Y")), "Delhi");
+const cityReplaced = city.replace(
+  city.slice(city.indexOf("y".toLocaleUpperCase())),
+  "Delhi"
+);
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
