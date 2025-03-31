589b90f182b5156292626bbccf71e8c8dfa4dea6
giuseppeesposito22 <gius.espo22@gmail.com>
2025-03-31 15:55:29 +0200

    string-bonus ex. 2

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -11,7 +11,7 @@ console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = "";
+const citySubstring = city.slice(city.indexOf("Y"));
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
